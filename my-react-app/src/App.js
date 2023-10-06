7cdc1fcf9901bd7fce7a0e6c60045834cfdae653
OkuhleM <lindokuhlea38@gmail.com>
2023-10-06 13:48:58 +0200

    everything

---
+++ my-react-app/src/App.js
@@ -0,0 +1,68 @@
+//This line imports the necessary modules from the 'react' library. 
+//It imports the React object and the useState hook. useState is used to manage the
+// state of variables in functional components.
+import React, { useState } from 'react';
+import './App.css';
+
+
+//This defines a functional component named App.
+
+function App() {
+
+
+  // This line declares a state variable name using the useState hook.
+  // Initially, name is set to an empty string, and setName is a function
+  // used to update the name state.
+//Similarly, there are state variables for age and email,
+// initialized to empty strings.
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [email, setEmail] = useState('');
+
+
+  //This is a function named testVariable. When called, it logs the values of
+  // name, age, and email to the console.
+  const testVariable = () => {
+    console.log('Input value', name, age, email);
+  };
+
+
+  //The value and onChange attributes of the input fields are used to bind 
+  //the input values to the corresponding state variables (name, age, email).
+  return (
+    <form className="form-background">
+      <div className="App">
+        <h1>React form</h1>
+
+        <div className="Form">
+          <label>Name</label><br></br>
+          <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </div>
+
+        <div className="Form">
+          <label>Age</label><br></br>
+          <input id="age" type="number" value={age} onChange={(e) => setAge(e.target.value)} />
+        </div>
+
+        <div className="Form">
+          <label>Email</label><br></br>
+          <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+
+        <button type="button" className='button' onClick={testVariable}>Submit</button>
+
+         {/* Display the state values */}
+         <div>
+          <p>Name: {name}</p>
+          <p>Age: {age}</p>
+          <p>Email: {email}</p>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+// This exports the App component as the default export of this module, making it available for use in
+// other parts of your React application.
+export default App;
+
